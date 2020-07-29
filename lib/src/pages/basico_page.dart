f476da6fdcebb6ade27d8b79e8980ad73c1a3736
wlachoo <w.edgardo22@gmail.com>
2020-07-29 13:30:20 -0400

    aplicaion disenos lista

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,13 +1,113 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
-  const BasicoPage({Key key}) : super(key: key);
+  
+  final estiloTitulo    = TextStyle( fontSize: 20.0, fontWeight: FontWeight.bold );
+  final estiloSubTitulo = TextStyle( fontSize: 18.0, color: Colors.grey );
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola mundo!!'),)
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(context),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+            _crearTexto(),
+          ],
+        ),
+      )
     );
   }
+
+  Widget _crearImagen(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: GestureDetector( // Un widget que detecta gestos, es decir si tocas click te lleva a una page
+        onTap: ()=> Navigator.pushNamed(context, 'scroll'), // ontap lo que quiere que haga cuando detecta un gesto
+        child: Image(
+          image: NetworkImage('https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+          height: 200.0,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: estiloTitulo ),
+                  SizedBox( height: 7.0 ),
+                  Text('Un lago en Alemania', style: estiloSubTitulo ),
+                ],
+              ),
+            ),
+
+            Icon( Icons.star, color: Colors.red, size: 30.0 ),
+            Text('41', style: TextStyle( fontSize: 20.0 ) )
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearAcciones() {
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+
+        _accion( Icons.call, 'CALL' ),
+        _accion( Icons.near_me, 'ROUTE' ),
+        _accion( Icons.share, 'Share'),
+
+      ],
+    );
+
+  }
+
+  Widget _accion(IconData icon, String texto ) {
+
+    return Column(
+      children: <Widget>[
+        Icon( icon, color: Colors.blue, size: 40.0 ),
+        SizedBox( height: 5.0 ),
+        Text( texto, style: TextStyle( fontSize: 15.0, color: Colors.blue ), )
+      ],
+    );
+
+  }
+
+  Widget _crearTexto() {
+
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric( horizontal: 40.0 ),
+        child: Text(
+          'Sit minim aliqua minim laborum duis occaecat consectetur aliquip id ad deserunt. Adipisicing qui reprehenderit eu eu qui occaecat exercitation et aliqua laboris dolor. Nisi duis consectetur veniam id nulla deserunt aliqua velit ullamco. Deserunt exercitation adipisicing nostrud amet eu.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
+    );
+
+  }
+
 }
